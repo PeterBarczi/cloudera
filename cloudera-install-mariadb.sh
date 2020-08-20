459e498324064ec71000308276249c3f971933cc
Pety <35726295+PeterBarczi@users.noreply.github.com>
2020-08-20 13:45:37 +0200

    password changed

--- cloudera-install-mariadb.sh
+++ cloudera-install-mariadb.sh
@@ -29,11 +29,11 @@ CREATE DATABASE oozie;
 CREATE DATABASE hue;
 CREATE DATABASE reportmanager;
 
-CREATE USER 'hive'@'%' IDENTIFIED BY 'itversity';
-CREATE USER 'oozie'@'%' IDENTIFIED BY 'itversity';
-CREATE USER 'hue'@'%' IDENTIFIED BY 'itversity';
-CREATE USER 'rm'@'%' IDENTIFIED BY 'itversity';
-CREATE USER 'scm'@'%' IDENTIFIED BY 'itversity';
+CREATE USER 'hive'@'%' IDENTIFIED BY 'cloudera';
+CREATE USER 'oozie'@'%' IDENTIFIED BY 'cloudera';
+CREATE USER 'hue'@'%' IDENTIFIED BY 'cloudera';
+CREATE USER 'rm'@'%' IDENTIFIED BY 'cloudera';
+CREATE USER 'scm'@'%' IDENTIFIED BY 'cloudera';
 
 GRANT ALL PRIVILEGES ON scm.* TO 'scm'@'%';
 GRANT ALL PRIVILEGES ON hive.* TO 'hive'@'%';
