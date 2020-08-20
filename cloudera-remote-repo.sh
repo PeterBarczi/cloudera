2ec6423aa7b73a5f652f1cc8a6f8a4c611917401
PeterBarczi <pety.barczi@gmail.com>
2020-08-20 10:19:16 +0200

    content addded

--- cloudera-remote-repo.sh
+++ cloudera-remote-repo.sh
@@ -1,12 +1,14 @@
 # This Script configures server as YUM Repository for CM and CDH
 
+# Define Variables
+server=infranode2.dual.edu
 # Install all necessary packages
 sudo yum -y install wget createrepo yum-utils httpd
 
 # Enable httpd
 sudo systemctl enable httpd --now
 
-# Cloudera Manager Part
+## Cloudera Manager Part
 cd /etc/yum.repos.d/
 sudo wget https://archive.cloudera.com/cm5/redhat/7/x86_64/cm/cloudera-manager.repo
 # Prepare directories
@@ -23,3 +25,13 @@ cd /var/www/html/cm5/redhat/7/x86_64/cm/
 sudo wget https://archive.cloudera.com/cm5/redhat/7/x86_64/cm/RPM-GPG-KEY-cloudera
 
 
+
+sudo cat <<EOF > /etc/yum.repos.d/cloudera-manager.repo 
+[cloudera-manager]
+# Packages for Cloudera Manager, Version 5, on RedHat or CentOS 7 x86_64
+name=Cloudera Manager
+baseurl=http://$server/cm5/redhat/7/x86_64/cm/5/
+gpgkey =http://$server/cm5/redhat/7/x86_64/cm/RPM-GPG-KEY-cloudera
+gpgcheck = 1
+EOF
+## End of Cloudera Manager Part
