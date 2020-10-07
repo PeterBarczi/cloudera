f877abdba29a306cff478c680e16d0c6b407dc91
root <root@infranode.dual.edu>
2020-10-07 16:05:08 +0200

    xx

--- cloudera-install-cm.sh
+++ cloudera-install-cm.sh
@@ -4,16 +4,19 @@ clear
 ## Install necessary packages
 # JAVA
 echo "Installing JAVA"
-sudo yum install oracle-j2sdk1.7 -y
+sudo yum -y install oracle-j2sdk1.7 -y
 echo "JAVA installed."
 # mysql-connector-java
-sudo yum install mysql-connector-java
+sudo yum -y install mysql-connector-java
 
 # Install Cloudera Manager
 sudo yum -y install cloudera-manager-server
 # Configure DB
 sudo systemctl stop cloudera-scm-server
+#CM5
 /usr/share/cmf/schema/scm_prepare_database.sh mysql -h localhost scm root cloudera
+#CM6
+/opt/cloudera/cm/schema/scm_prepare_database.sh mysql -h localhost scm root cloudera
 # Start CM
 sudo systemctl enable cloudera-scm-server.service
 sudo systemctl start cloudera-scm-server.service
